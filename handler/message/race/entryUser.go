24d48fd739b34e7e091a970ff5f892c49bfd1423
totsuka <techstart35@gmail.com>
2023-09-23 00:10:02 +0900

    順番を修正

--- handler/message/race/entryUser.go
+++ handler/message/race/entryUser.go
@@ -31,9 +31,9 @@ func (e *EntryUser) AddPoint(point int) {
 
 // 全てのエントリーユーザーです
 var EntryUsers = []EntryUser{
-	{Emoji: Emoji1, Name: Name1, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549274875576373/RGR_A_winner.png"},
 	{Emoji: Emoji2, Name: Name2, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1154791795696349184/RGR_F_winner.png"},
-	{Emoji: Emoji3, Name: Name3, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1154792056368136322/RGR_G_winner.png"},
-	{Emoji: Emoji4, Name: Name4, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549746613125130/RGR_D_winner.png"},
 	{Emoji: Emoji5, Name: Name5, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549771523117066/RGR_E_winner.png"},
+	{Emoji: Emoji4, Name: Name4, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549746613125130/RGR_D_winner.png"},
+	{Emoji: Emoji3, Name: Name3, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1154792056368136322/RGR_G_winner.png"},
+	{Emoji: Emoji1, Name: Name1, Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549274875576373/RGR_A_winner.png"},
 }
