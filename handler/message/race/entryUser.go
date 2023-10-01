fc86064f1746ada338d8bb4136260bba2fc6b700
totsuka <techstart35@gmail.com>
2023-10-01 19:23:17 +0900

    絵文字を修正

--- handler/message/race/entryUser.go
+++ handler/message/race/entryUser.go
@@ -18,7 +18,7 @@ const EntryImageURL = "https://cdn.discordapp.com/attachments/110324022337629393
 // 全てのエントリーユーザーです
 var EntryUsers = []EntryUser{
 	{Emoji: "☎️", Name: "ピンクテレフォン50", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1157982286445281300/RGR_H_winner.png"},
-	{Emoji: "⭐️", Name: "スタースター", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549771523117066/RGR_E_winner.png"},
+	{Emoji: "⭐", Name: "スタースター", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549771523117066/RGR_E_winner.png"},
 	{Emoji: "🐟", Name: "アロワナー", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549746613125130/RGR_D_winner.png"},
 	{Emoji: "🚔", Name: "メカコップ", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1157982487553789962/RGR_I_winner.png"},
 	{Emoji: "🔥", Name: "スピットファイア", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549274875576373/RGR_A_winner.png"},
