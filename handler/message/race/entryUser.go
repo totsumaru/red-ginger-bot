7764986642d160fbb8dda88c952c33448c551f49
totsuka <techstart35@gmail.com>
2023-10-10 12:46:37 +0900

    新シーズンを追加

--- handler/message/race/entryUser.go
+++ handler/message/race/entryUser.go
@@ -13,13 +13,15 @@ func (e *EntryUser) AddPoint(point int) {
 	e.Point += point
 }
 
-const EntryImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1157982956003012638/RGGandPrix3.png"
+const EntryImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1161143998275145738/RGGandPrix4.png"
 
 // 全てのエントリーユーザーです
+//
+// ⭐️は、絵文字の後ろの1文字(空文字)を削除するとうまくいきます。
 var EntryUsers = []EntryUser{
-	{Emoji: "☎️", Name: "ピンクテレフォン50", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1157982286445281300/RGR_H_winner.png"},
-	{Emoji: "⭐", Name: "スタースター", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549771523117066/RGR_E_winner.png"},
 	{Emoji: "🐟", Name: "アロワナー", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549746613125130/RGR_D_winner.png"},
+	{Emoji: "🦉", Name: "エイジアンプライド", Point: 0, ImageURL: "https://media.discordapp.net/attachments/1103240223376293938/1161143904117198919/RGR_J_winner.png"},
 	{Emoji: "🚔", Name: "メカコップ", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1157982487553789962/RGR_I_winner.png"},
+	{Emoji: "⭐", Name: "スタースターバスター", Point: 0, ImageURL: "https://media.discordapp.net/attachments/1103240223376293938/1161143945733083186/RGR_K_winner.png"},
 	{Emoji: "🔥", Name: "スピットファイア", Point: 0, ImageURL: "https://cdn.discordapp.com/attachments/1103240223376293938/1149549274875576373/RGR_A_winner.png"},
 }
